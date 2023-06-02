d701861cd382affb61f926a2c35e6314586542b8
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-06-01 21:46:26 -0700

    Complete knights_moves method with tests

--- index.rb
+++ index.rb
@@ -1,1 +1,39 @@
-# TODO
+require 'set'
+
+def is_valid_cell?(cell, board_length = 8)
+  row, col = cell
+  return false if row.class != Integer or col.class != Integer
+
+  row >= 0 && row < board_length && col >= 0 && col < board_length
+end
+
+def knights_moves(start_cell, end_cell, board_length = 8)
+  return [] if !is_valid_cell?(start_cell) or !is_valid_cell?(end_cell)
+  return [start_cell, end_cell] if start_cell == end_cell
+
+  directions = [
+    [-1, -2],   # 2 lefts -> 1 up
+    [1, -2],    # 2 lefts -> 1 down
+    [-1, 2],    # 2 rights -> 1 up
+    [1, 2],     # 2 rights -> 1 down
+    [-2, -1],   # 2 ups -> 1 left
+    [-2, 1],    # 2 ups -> 1 right
+    [2, -1],    # 2 downs -> 1 left
+    [2, 1]      # 2 downs -> 1 right
+  ]
+  visit = Set.new                       # set of cells
+  queue = [[start_cell, [start_cell]]]  # queue of [cell, path]
+
+  until queue.empty?
+    cell, path = queue.shift
+    return path if cell == end_cell
+
+    directions.each do |dir|
+      new_cell = [cell[0] + dir[0], cell[1] + dir[1]]
+      next if !is_valid_cell?(new_cell) or visit.include?(new_cell)
+
+      queue.push([new_cell, [*path, new_cell]])
+      visit.add(new_cell)
+    end
+  end
+end
--- index.spec.rb
+++ index.spec.rb
@@ -1,11 +1,54 @@
 require_relative 'spec_helper'
 require_relative 'index'
 
-RSpec.describe 'index' do
-  describe "TODO" do
-    it "TODO" do
-      # TODO
-      expect(true).not_to eq(nil)
+RSpec.describe "index" do
+  describe "knights_move" do
+    it "returns an empty array if called with ([-1,-1], [0,0])" do
+      res = knights_moves([-1,-1], [0,0])
+      expect(res).to eq([])
+    end
+
+    it "returns an empty array if called with ([0,0], ['a','b'])" do
+      res = knights_moves([-1,-1], ['a','b'])
+      expect(res).to eq([])
+    end
+
+    it "returns an [[0,0],[0,0]] if called with ([0,0], [0,0])" do
+      res = knights_moves([0,0], [0,0])
+      expect(res).to eq([[0,0],[0,0]])
+    end
+
+    it "returns an [[0,0],[1,2]] if called with ([0,0], [1,2])" do
+      res = knights_moves([0,0], [1,2])
+      expect(res).to eq([[0,0],[1,2]])
+    end
+
+    it "returns one of the valid answers if called with ([0,0], [3,3])" do
+      expected = Set.new([
+        [[0,0], [1,2], [3,3]],
+        [[0,0], [2,1], [3,3]],
+      ])
+      res = knights_moves([0,0], [3,3])
+      expect(res.size).to eq(3)
+      expect(expected.include?(res)).to eq(true)
+    end
+
+    it "returns one of the valid answers if called with ([3,3], [0,0])" do
+      expected = Set.new([
+        [[3,3], [1,2], [0,0]],
+        [[3,3], [2,1], [0,0]],
+      ])
+      res = knights_moves([3,3], [0,0])
+      expect(res.size).to eq(3)
+      expect(expected.include?(res)).to eq(true)
+    end
+
+    it "returns one of the valid answers if called with ([3,3], [4,3])" do
+      res = knights_moves([3,3], [4,3])
+      puts("\n#{res.to_s}")
+      expect(res.size).to eq(4)
+      expect(res[0]).to eq([3,3])
+      expect(res[-1]).to eq([4,3])
     end
   end
 end
